2ea686be504553cb049c64d144e794d52c0fef0d
Vince Andrew Pimentel <contact@vincep.me>
2018-10-19 21:40:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,8 +68,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, decoration|
-      puts "#{season.to_s}:"
-      puts "  #{holiday.to_s}: #{decoration}"
+      puts "#{season.to_s.capitalize}:"
+      puts "  #{holiday.to_s.capitalize}: #{decoration}"
     end
   end
 end
