f1b207a389ca56bb1239bc50034100831b26847d
Vince Andrew Pimentel <contact@vincep.me>
2018-10-19 21:20:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, decorations|
-      if holiday == :christmas && holiday == :new_years
+      if holiday == :christmas || holiday == :new_years
         decorations << supply
       end
     end
