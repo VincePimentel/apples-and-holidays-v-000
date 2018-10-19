d8d6ec2bb8b7ed0373072e2e9b21628e6a5048e4
Vince Andrew Pimentel <contact@vincep.me>
2018-10-19 21:38:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, decoration|
+      puts "#{season}:"
+      puts "  #{holiday}: #{decoration}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
